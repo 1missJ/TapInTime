bfecf6179cb42c1dfedb9b74c9f1a235242beaae
Yadan <163361578+Yadan@users.noreply.github.com>
2025-03-31 22:30:11 +0800

    Add files via upload

---
+++ Taptrack12/database/tapintime.sql
@@ -0,0 +1,171 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 31, 2025 at 03:26 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tapintime`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pending_students`
+--
+
+CREATE TABLE `pending_students` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `middle_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `lrn` varchar(12) NOT NULL,
+  `date_of_birth` date NOT NULL,
+  `gender` enum('Male','Female') NOT NULL,
+  `citizenship` varchar(50) NOT NULL,
+  `address` text NOT NULL,
+  `contact_number` varchar(15) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `section` varchar(50) NOT NULL,
+  `school_year` varchar(9) NOT NULL,
+  `guardian_name` varchar(100) NOT NULL,
+  `guardian_contact` varchar(15) NOT NULL,
+  `guardian_address` varchar(250) NOT NULL,
+  `guardian_relationship` varchar(50) NOT NULL,
+  `elementary_school` varchar(100) NOT NULL,
+  `year_graduated` year(4) NOT NULL,
+  `birth_certificate` varchar(255) DEFAULT NULL,
+  `id_photo` varchar(255) DEFAULT NULL,
+  `good_moral` varchar(255) DEFAULT NULL,
+  `student_signature` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `middle_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `lrn` varchar(12) NOT NULL,
+  `date_of_birth` date NOT NULL,
+  `gender` enum('Male','Female') NOT NULL,
+  `citizenship` varchar(50) NOT NULL,
+  `address` text NOT NULL,
+  `contact_number` varchar(15) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `guardian_name` varchar(100) NOT NULL,
+  `guardian_contact` varchar(15) NOT NULL,
+  `guardian_address` varchar(250) NOT NULL,
+  `guardian_relationship` varchar(50) NOT NULL,
+  `elementary_school` varchar(100) NOT NULL,
+  `year_graduated` year(4) NOT NULL,
+  `birth_certificate` varchar(255) NOT NULL,
+  `id_photo` varchar(255) NOT NULL,
+  `good_moral` varchar(255) DEFAULT NULL,
+  `student_signature` varchar(255) NOT NULL,
+  `section` varchar(50) DEFAULT NULL,
+  `school_year` varchar(9) NOT NULL,
+  `verified_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `first_name`, `middle_name`, `last_name`, `lrn`, `date_of_birth`, `gender`, `citizenship`, `address`, `contact_number`, `email`, `guardian_name`, `guardian_contact`, `guardian_address`, `guardian_relationship`, `elementary_school`, `year_graduated`, `birth_certificate`, `id_photo`, `good_moral`, `student_signature`, `section`, `school_year`, `verified_at`, `created_at`) VALUES
+(25, 'Marvita Marie Elmarie', 'Dela Cruz', 'Cataggatan', '120000000000', '2025-03-27', 'Female', 'Filipino', 'Casibarag Sur, Cabagan, Isabela', '12345678912', 'marvita.2003@gmail.com', 'Oasdsfsg fdsforegs fdaslfdsgfr fk;dsaf', '09111111111', 'Casibarag Sur, Cabagan, Isabela', 'Father', 'CSES', '2016', 'uploads/67ea512c1a2e7_birth_certificate.jpg', 'uploads/67ea512c1fb18_id_photo.jpg', 'uploads/67ea512c23488_good_moral.png', 'uploads/67ea512c23f75_student_signature.jpg', 'Mabini', '2025-2029', '2025-03-31 08:25:18', '2025-03-31 08:25:18');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin') DEFAULT 'admin',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `role`, `created_at`) VALUES
+(1, 'admin', 'admin', 'admin', '2025-02-20 15:02:38');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pending_students`
+--
+ALTER TABLE `pending_students`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `lrn` (`lrn`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `lrn` (`lrn`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pending_students`
+--
+ALTER TABLE `pending_students`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
